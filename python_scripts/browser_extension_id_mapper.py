1ec5ead39f55a984ef731292af9eb3bafb496936
drewski13 <shane.h.andrews@gmail.com>
2022-11-23 20:18:55 -0500

    chrome IDs mapped, prints pretty JSON. next is CSV outfile

--- python_scripts/browser_extension_id_mapper.py
+++ python_scripts/browser_extension_id_mapper.py
@@ -9,9 +9,11 @@
 
 #imports
 import argparse
+import urllib.request
 from datetime import datetime
 import os
 import re
+import json
 
 
 #GLOBALS
@@ -27,22 +29,24 @@ o888ooooood8 o88'   888o   "888"    o888o o888bood8P'      o8o        o888o `Y88
                                                                                       o888o      o888o
 '''
 skeleton_path_to_chrome_extensions = "C:\\Users\\{user}\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Extensions"
+chrome_baseURL = 'https://chrome.google.com/webstore/detail/'
+edge_baseURL = 'https://microsoftedge.microsoft.com/addons/detail/'
 
 
 #FUNCTIONS
-def get_datetime():
+def get_datetime() -> str:
     now = datetime.now()
     dt_string = now.strftime("%d%b%Y_%H.%M")
     return dt_string
 
 
-def get_users_folderpaths():
+def get_users_folderpaths() -> list:
     subfolders = [f.path for f in os.scandir("C:\\users") if f.is_dir()]
     only_users_foldernames = [get_last_foldername(folder) for folder in subfolders]
     return only_users_foldernames
 
 
-def get_exts_from_folderpath(ext_folder):
+def get_exts_from_folderpath(ext_folder: str) -> list:
     if not os.path.exists(ext_folder):
         return
     subfolders = [f.path for f in os.scandir(ext_folder) if f.is_dir()]
@@ -50,18 +54,68 @@ def get_exts_from_folderpath(ext_folder):
     return only_ext_names
 
 
-def get_last_foldername(full_path):
+def get_last_foldername(full_path: str) -> str:
     return re.sub(".*\\\\", "", full_path)
 
 
-def local_run(outfile):
-    #dict for mapping local users to their extensions
-    user_ext_dict = {}
+# helper function to return a string of the webpage returned by the method to the URL
+#relies on IMPORTing urllib.request
+def get_to_url(url: str, method: str) -> str:
+    try:
+        req = urllib.request.Request(url, method=method)
+        with urllib.request.urlopen(req) as response:
+            return response.read().decode('utf-8')
+    except Exception as e:
+        return ""
+
+
+def map_list_of_ext_ids(list_of_ext_ids: list, browser: str) -> dict:
+    ret_dict = {}
+    if browser == 'chrome':
+        for ext_id in list_of_ext_ids:
+            ret_dict[ext_id] = map_chrome_ext_id(ext_id)
+    return ret_dict
+
+
+def map_chrome_ext_id(ext_id: str) -> dict:
+    full_webpage = get_to_url(f"{chrome_baseURL}{ext_id}", "GET")
+    if not full_webpage:
+        return {'name': "unknown", 'description': "no page reached for this ext ID"}
+    else:
+        ext_name_match = re.match(r'(?i).*\<title\>([^\<]+)\<\/title\>.*', full_webpage)
+        if ext_name_match:
+            ext_name = ext_name_match[1]
+        else:
+            ext_name = "no ext name found with regex"
+
+        #slicing out data here just because it seems to give me more consistently correct results in testing
+        desc_match = re.match(r'(?i).*description.?\>[^\<]+\<[^\>]+\>\<[^\>]+\>([^\<]*)\<\/.*', full_webpage[1000:])
+        if ext_name_match:
+            ext_desc = desc_match[1].replace("\n", "")
+        else:
+            ext_desc = "no ext description found with regex"
+    return {'name': ext_name, 'description': ext_desc}
+
+
+def local_run(outfile: str):
+    # starting with the logic for chrome extension ID mapping. NOTE: a simple urllib request can be used for these
+    # dict for mapping local users to their extensions
+    user_chrome_ext_dict = {}
     outfile = outfile
     list_of_users = get_users_folderpaths()
     for user in list_of_users:
         ext_names = get_exts_from_folderpath(skeleton_path_to_chrome_extensions.format(user=user))
-    print(ext_names)
+        user_chrome_ext_dict[user] = ext_names
+
+    # logic for getting the names/descriptions from the chrome store
+    for user, chrome_ext_list in user_chrome_ext_dict.items():
+        if chrome_ext_list is not None:
+            user_chrome_ext_dict[user] = map_list_of_ext_ids(chrome_ext_list, 'chrome')
+
+    # for testing purposes
+    json_output = json.dumps(user_chrome_ext_dict, indent=2)
+    print(f"JSON version of output:\n{json_output}")
+
 
 
 #MAIN
@@ -91,7 +145,7 @@ def main():
     if not outfile:
         dt = get_datetime()
         outfile = f"{dt}_{ext_file}_mapped.csv"
-    print(f"output will be placed in {outfile}")
+    print(f"[*] output will be placed in {outfile}")
 
     #logic for a local mapping, looking in all users for C:\users\<username>\AppData\Local\Google\Chrome\User Data\Default\Extensions
     if local_run_bool:
